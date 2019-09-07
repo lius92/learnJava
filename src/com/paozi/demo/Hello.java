f745c5492cf4948c445c7c03bf854b2e1e49455a
liushan <liushan@kaisagroup.com>
2019-09-07 15:32:55 +0800

    lianxi

--- src/com/paozi/demo/Hello.java
+++ src/com/paozi/demo/Hello.java
@@ -2,6 +2,37 @@ package com.paozi.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+        double x = 1.0 / 10;
+        double y = 1 - 9.0 / 10;   //计算有误差
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int n = 5;
+        // 参与计算的 24.0 / n  其中n是整数 则自动提升类型为浮点型 24.0 / 5.0 = 4.8
+        double m = 1.2 + 24.0 / n;
+        System.out.println(m);
+
+        int n1 = 'A';
+        char c3 = '\u0041';
+
+        System.out.println(n1);
+        System.out.println(c3);
+
+
+        String s = "hello";
+        String t = s;
+        s = "world";
+        System.out.println(t); // t是"hello"还是"world"?s
+
+        //
+
+        String[] names = {"ABC", "XYZ", "zoo"};
+        String sn = names[1];
+        names[1] = "cat";
+        System.out.println(sn);
+
     }
 }
